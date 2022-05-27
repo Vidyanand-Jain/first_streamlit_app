3c170db080db51d202ff4edcf60f5aed72b89bf0
Vidyanand-Jain <52501648+Vidyanand-Jain@users.noreply.github.com>
2022-05-27 12:21:54 +0200

    Update streamlit_app.py
    added details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title('My Parents New Healthy Dinerr')
 streamlit.header('Breakfast Menu')
-streamlit.text('Avalakki')
-streamlit.text('Uppittu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
