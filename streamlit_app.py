701c7bb5f25c78a19dec4d6f7b7558bd0e7c75c6
Vidyanand-Jain <52501648+Vidyanand-Jain@users.noreply.github.com>
2022-06-01 12:15:16 +0200

    Update streamlit_app.py
    added URLError

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-#from urlib.error import URLError
+from urlib.error import URLError
 
 streamlit.title('My MOM"s" New Healthy Diner')
 streamlit.header('Breakfast Menu')
